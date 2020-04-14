6d4ae5ae828167888f8371cf7f306282c0530f1b
Misha Balin <m.balin@icloud.com>
2020-04-14 14:54:17 +0300

    Add Layout and Footer components

---
+++ src/components/SiteLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <SiteFooter></SiteFooter>
+  </div>
+</template>
+
+<script>
+import SiteFooter from '@/components/SiteFooter.vue';
+
+export default {
+  components: {
+    SiteFooter
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>This is an home page</h1>
+    <SiteLayout></SiteLayout>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import SiteLayout from '@/components/SiteLayout.vue';
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    SiteLayout
+  }
 };
 </script>
