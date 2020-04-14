88615059dd49bcb9da15fa1942c36fe0cbbb41e5
Misha Balin <m.balin@icloud.com>
2020-04-14 18:12:31 +0300

    Add Game Block

---
+++ src/components/BlockGame.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-container>
+      <v-row align="center">
+        <v-col cols="6">
+          <v-img
+            :src="require('../assets/illustrations/players-fight.png')"
+            max-width="100%"
+          ></v-img>
+        </v-col>
+        <v-col cols="6">
+          <h2>Что это?</h2>
+          <p>
+            ЮКИГАССЕН — это зимняя командная игра, зрелищная и азартная, которая
+            зародилась в Японии около 20 лет назад, стала популярной в других
+            снежных странах и наконец пришла к нам в Санкт-Петербург.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlockGame'
+};
+</script>
+
+<style scoped></style>
