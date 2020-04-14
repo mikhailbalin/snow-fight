904cbeef44f671a088d6308d5ced7db5a58f4388
Misha Balin <m.balin@icloud.com>
2020-04-14 18:12:41 +0300

    Add Newsletter block

---
+++ src/components/BlockNewsletter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-container>
+      <h2>Будте в курсе последних событий!</h2>
+      <small>
+        Никакого спама, обещаем!
+        <router-link :to="{ name: 'Privacy' }">Конфиденциальность</router-link>
+      </small>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Newsletter'
+};
+</script>
+
+<style scoped></style>
