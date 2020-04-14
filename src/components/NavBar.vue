24acd10a2d17b6b8eaf11d72584d7f83bca81583
Misha Balin <m.balin@icloud.com>
2020-04-14 15:08:38 +0300

    Add NavBar add StatCard

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>NavBar</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ src/components/StatCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      card: {
+        id: 1,
+        image: '',
+        count: 3,
+        info: ''
+      }
+    };
+  }
+};
+</script>
+
+<style scoped></style>
