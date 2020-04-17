b1daa3fde5a33dc34440c76aa91a8882e5990bd6
Misha Balin <m.balin@icloud.com>
2020-04-17 18:34:14 +0300

    Add Contacts page

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -5,8 +5,7 @@
     <v-sheet tile color="grey lighten-5 fill-height grey--text text--darken-1">
       <v-container class="px-8 py-12">
         <v-row>
-          <!-- md="8" offset-md="2" lg="8" offset-lg="2" -->
-          <v-col cols="12">
+          <v-col cols="12" md="5" lg="3" offset-lg="2" xl="2" offset-xl="3">
             <address class="app-address mb-6">
               <strong class="mb-2">Снежный бой Юкигассен</strong><br />
               Ленинградская область<br />
@@ -24,7 +23,76 @@
             </address>
           </v-col>
 
-          <v-col cols="12"> </v-col>
+          <v-col cols="12" class="d-md-none">
+            <v-divider class="my-3" />
+          </v-col>
+
+          <v-col cols="12" md="7" lg="5" xl="4">
+            <v-form v-model="formValidity">
+              <v-row>
+                <v-col cols="12" sm="6" class="pb-0 pb-sm-3">
+                  <v-text-field
+                    label="Имя"
+                    type="text"
+                    placeholder="Петр Иванов"
+                    outlined
+                    dense
+                    height="44"
+                    :rules="nameRules"
+                    v-model="name"
+                    required
+                  />
+                </v-col>
+
+                <v-col cols="12" sm="6" class="py-0 py-sm-3">
+                  <v-text-field
+                    label="Почта"
+                    type="email"
+                    placeholder="example@mail.ru"
+                    outlined
+                    dense
+                    height="44"
+                    :rules="emailRules"
+                    v-model="email"
+                    required
+                  />
+                </v-col>
+
+                <v-col cols="12" class="pt-0 pt-sm-3">
+                  <v-textarea
+                    outlined
+                    name="message"
+                    label="Сообщение"
+                    v-model="message"
+                    :rules="messageRules"
+                    placeholder="Я бы хотел..."
+                    required
+                  ></v-textarea>
+                </v-col>
+
+                <v-col cols="12">
+                  <small>
+                    Нажимая <strong>Отправить</strong>, вы соглашаетесь с
+                    <router-link :to="{ name: 'Privacy' }">
+                      политикой конфиденциальности.
+                    </router-link>
+                  </small>
+
+                  <br />
+
+                  <v-btn
+                    class="mt-5"
+                    large
+                    type="submit"
+                    color="primary"
+                    :disabled="!formValidity"
+                  >
+                    Отправить
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-col>
         </v-row>
       </v-container>
     </v-sheet>
@@ -38,7 +106,26 @@ import { mapState } from 'vuex';
 export default {
   name: 'Contacts',
   components: { AppBanner },
-  computed: mapState(['tel'])
+  computed: mapState(['tel']),
+  data: () => ({
+    name: '',
+    nameRules: [value => !!value || 'Введите вашe имя.'],
+    email: '',
+    emailRules: [
+      value => !!value || 'Введите вашу почту.',
+      value => value.indexOf('@') !== 0 || 'Email should have a username.',
+      value => value.includes('@') || 'Email should include @ symbol.',
+      value =>
+        value.indexOf('.') - value.indexOf('@') > 1 ||
+        'Email shuold contain a valid domain.',
+      value =>
+        value.indexOf('.') <= value.length - 3 ||
+        'Email should contain a valid domain extension.'
+    ],
+    message: '',
+    messageRules: [value => !!value || 'Введите сообщение.'],
+    formValidity: false
+  })
 };
 </script>
 
