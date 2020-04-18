6f1f5c0cbe85606415b019bd4085329558059b52
Misha Balin <m.balin@icloud.com>
2020-04-18 20:44:50 +0300

    Add hcaptcha

---
+++ src/components/AppCaptcha.vue
@@ -0,0 +1,14 @@
+<template>
+  <vue-hcaptcha :sitekey="process.env.VUE_APP_CAPTCHA_SITE_KEY"></vue-hcaptcha>
+</template>
+
+<script>
+import VueHcaptcha from '@hcaptcha/vue-hcaptcha';
+
+export default {
+  name: 'AppCaptcha',
+  components: { VueHcaptcha }
+};
+</script>
+
+<style lang="scss" scoped></style>
