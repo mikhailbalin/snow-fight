714a8af1439fe8feb8d8eff61a01fb531b150157
Misha Balin <m.balin@icloud.com>
2020-05-15 12:30:59 +0300

    Test req

--- api/post-email.js
+++ api/post-email.js
@@ -1,6 +1,7 @@
 const verifyCaptcha = require('./_utils/verifyCaptcha');
 
 module.exports = ({ body: reqData }, res) => {
+  console.log({ reqData });
   const { status, data } = verifyCaptcha(reqData);
   res.status(status).send(data);
 };
