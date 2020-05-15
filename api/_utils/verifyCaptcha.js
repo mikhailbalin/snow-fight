eaaf68603c3d0defd7111e148235d4cd80d652bb
Misha Balin <m.balin@icloud.com>
2020-05-15 13:23:43 +0300

    Test

--- api/_utils/verifyCaptcha.js
+++ api/_utils/verifyCaptcha.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 const { verify } = require('hcaptcha');
 const getUrl = require('./getUrl');
 
-const getResponse = (status, data) => ({ status, data });
+// const getResponse = (status, data) => ({ status, data });
 
 module.exports = reqData => {
   const { token, ...values } = reqData;
@@ -18,21 +18,26 @@ module.exports = reqData => {
             .post(getUrl('snow_fight_email_post'), values)
             .then(res => {
               console.log('axios res', res);
-              return getResponse(200, 'axios res');
+              return { status: 200, data: 'axios res' };
+              // getResponse(200, 'axios res');
             })
             .catch(err => {
               console.error('axios err', err);
-              return getResponse(500, 'axios error');
+              return { status: 500, data: 'axios err' };
+              // getResponse(500, 'axios error');
             });
         } else {
-          return getResponse(500, 'captcha error');
+          return { status: 500, data: 'captcha error' };
+          // getResponse(500, 'captcha error');
         }
       })
       .catch(err => {
         console.error('verify err', err);
-        return getResponse(500, 'verify error');
+        return { status: 500, data: 'verify error' };
+        // getResponse(500, 'verify error');
       });
   } else {
-    return getResponse(500, 'No email token provided');
+    return { status: 500, data: 'no token' };
+    // return getResponse(500, 'no token provided');
   }
 };
