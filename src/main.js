a94a1117e61a73665b8b809c7a56dc9455169d6f
Misha Balin <m.balin@icloud.com>
2020-06-02 13:18:09 +0300

    Fix VueGtag config

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import VueGtag from 'vue-gtag';
 
-Vue.use(VueGtag, {
-  config: { id: 'G-0KZLLH1NS2' }
-});
+Vue.use(
+  VueGtag,
+  {
+    config: { id: 'G-0KZLLH1NS2' }
+  },
+  router
+);
 
 Vue.config.productionTip = false;
 
