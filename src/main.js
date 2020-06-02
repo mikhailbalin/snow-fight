ec87a7e42c8fcfe77e710fc1bacc5f43e059e800
Misha Balin <m.balin@icloud.com>
2020-06-02 13:14:26 +0300

    Config analitics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import VueGtag from 'vue-gtag';
+
+Vue.use(VueGtag, {
+  config: { id: 'G-0KZLLH1NS2' }
+});
 
 Vue.config.productionTip = false;
 
